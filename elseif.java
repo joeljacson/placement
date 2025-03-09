a0edb453b08198f256aa884b27c778809e710e30
JOEL JACSON.J <166722242+joeljacson@users.noreply.github.com>
2025-03-09 16:41:01 +0530

    esleif code is executed

---
+++ elseif.java
@@ -0,0 +1,24 @@
+public class elseif {
+
+    public static void main (String[]args)
+
+    {
+        int num=77;
+
+        if(num>35&& num<60)
+        {
+            System.out.print("video game");
+        }
+        
+        else if(num>60&&num<90)
+        {
+            System.out.print("Iphone");
+        
+        }
+       else if(num>90)
+        {
+            System.out.print("Tesla");
+        }
+    }
+    
+}
