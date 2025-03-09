70dc904342c5b245392c594a483efa4521355af8
JOEL JACSON.J <166722242+joeljacson@users.noreply.github.com>
2025-03-09 15:45:15 +0530

    Add files via upload
    the code is executed

---
+++ mod.java
@@ -0,0 +1,18 @@
+ 
+ class mod {
+
+    public static void main (String args[])
+    {
+        int num=11;
+    if(num%3==0)
+    {
+        System.out.print("15 divisble by 3");
+    }   
+    else{
+        System.out.print("15 is not divisble by 3");
+    }
+    
+    }
+
+    
+}
