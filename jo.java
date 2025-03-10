6f68364d73e8f2589abbd5af2e0b060939360aae
joel jacson <jamesjoel7114@gmail.com>
2025-03-10 10:24:45 +0530

    jo code changed

---
+++ jo.java
@@ -0,0 +1,13 @@
+public class CareerPath {
+    public static void main(String[] args) {
+        String careerChoice = "python"; // Change this to "Cloud" to test
+
+        if (careerChoice.equals("Java")) {
+            System.out.println("Joel is going for a Java Developer job.");
+        } else if (careerChoice.equals("Cloud")) {
+            System.out.println("Joel is going for a Cloud Engineer job.");
+        } else {
+            System.out.println("Joel is exploring other career options.");
+        }
+    }
+}
